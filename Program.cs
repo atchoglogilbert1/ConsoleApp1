a507d66b4b114d2cc98432a50d78ed3c1cbed2cc
Kossivi G. Atchoglo <92089944+atchoglogilbert@users.noreply.github.com>
2026-09-14 01:35:21 +0200

    add AI code slope and intercept, cause TO

--- Program.cs
+++ Program.cs
@@ -1,32 +1,32 @@
 ﻿using System;
-using System.Data;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Runtime.ConstrainedExecution;
-using System.Runtime.InteropServices;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
-
-    // function to convert csv data -> dictionary<int,int>
-
-    public static Dictionary<int, int> Read (String filename, bool log = false)
+    // Function to convert CSV data -> Dictionary<int, int>
+    public static Dictionary<int, int> Read(string filename, bool log = false)
     {
-
         try
         {
             string[] rows = File.ReadAllLines(filename);
             Dictionary<int, int> data = new Dictionary<int, int>();
+
             foreach (string row in rows.Skip(1))
             {
-                
                 string[] explosion = row.Split(',');
+
                 data[int.Parse(explosion[0])] = int.Parse(explosion[1]);
+
                 if (log)
                 {
                     Console.WriteLine(explosion[0] + ":" + explosion[1]);
                 }
             }
-            return (data);
+
+            return data;
         }
         catch (FileNotFoundException)
         {
@@ -35,30 +35,62 @@ class Program
         }
     }
 
+
+    // Calculates the intercept:
+    // β̂₀ = Ȳ - β̂₁x̄
     public static double intercept(Dictionary<int, int> data)
     {
+        double xMean = data.Keys.Average();
+        double yMean = data.Values.Average();
 
-        
+        double slopeValue = slope(data);
+
+        return yMean - slopeValue * xMean;
     }
 
-    /// Finds the slope.
+
+    // Calculates the slope:
+    // β̂₁ = Σ(xᵢ - x̄)(Yᵢ - Ȳ) / Σ(xᵢ - x̄)²
     public static double slope(Dictionary<int, int> data)
     {
+        double xMean = data.Keys.Average();
+        double yMean = data.Values.Average();
 
+        double numerator = 0;
+        double denominator = 0;
 
+        foreach (var pair in data)
+        {
+            double x = pair.Key;
+            double y = pair.Value;
+
+            numerator += (x - xMean) * (y - yMean);
+            denominator += Math.Pow(x - xMean, 2);
+        }
+
+        return numerator / denominator;
     }
 
 
     public static void Main(string[] args)
     {
+        // Developer constants
+        const bool LOG = true; 
+
         const string fileName = "data.csv";
-        Dictionary<int,int> data = Read (fileName, false);
 
-        
+        Dictionary<int, int> data = Read(fileName, LOG);
 
+        double slopeValue = slope(data);
+        double interceptValue = intercept(data);
 
-
-         
-    
+        if (LOG)
+        {
+            Console.WriteLine("Slope: " + slopeValue);
+            Console.WriteLine("Intercept: " + interceptValue);
+            Console.WriteLine(
+                $"Regression equation: ŷ = {interceptValue} + {slopeValue}x"
+            );
+        }
     }
 }
