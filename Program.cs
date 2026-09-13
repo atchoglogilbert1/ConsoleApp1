e6da4c474b9b3c8a5698d496c4d598f6db144095
Kossivi G. Atchoglo <92089944+atchoglogilbert@users.noreply.github.com>
2026-09-14 00:39:12 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+
+class Program
+{
+
+    public static void  
+
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
